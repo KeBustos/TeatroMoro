bc06d3a1f8e7ea8f9cf164d516faed3c0b0090ec
Kevin Bustos <ke.bustoss@duocuc.cl>
2025-04-02 16:09:15 -0300

    Commit inicial

---
+++ src/main/java/com/mycompany/teatromorov2/TeatroMoroV2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.teatromorov2;
+
+/**
+ *
+ * @author kevin
+ */
+public class TeatroMoroV2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
